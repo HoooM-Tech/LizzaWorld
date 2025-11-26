277d64c10bf7d9cfcf2d2c7417de2c3a7c7f3196
Librapraise <alabipraise26@gmail.com>
2025-11-26 11:40:27 +0100

    update: hero video

--- app/page.tsx
+++ app/page.tsx
@@ -15,9 +15,7 @@ export default async function HomePage() {
   ]);
 
   // For video files, access the asset URL directly
-  const heroVideo = homeData?.heroMedia?.asset?.url || homeData?.heroMedia?.asset?._ref 
-    ? `https://cdn.sanity.io/files/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${process.env.NEXT_PUBLIC_SANITY_DATASET}/${homeData.heroMedia.asset._ref.replace('file-', '').replace('-mp4', '.mp4').replace('-webm', '.webm').replace('-mov', '.mov')}`
-    : undefined;
+  const heroVideo = homeData?.heroMedia?.asset?.url;
 
   const featuredVisuals = homeData?.featuredVisuals?.map((image: unknown) => urlFor(image).url());
   const testimonials = homeData?.testimonials?.map((testimonial: any) => ({
